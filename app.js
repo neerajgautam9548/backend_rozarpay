c843aa6d8e7b8428f77b925fe6b433e5067ca74e
neeraj gautam <neerajgautam@gmail.com>
2026-03-17 19:01:10 +0530

    add folder 2nd time

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 // app.js
-const express = require('express');
-const Razorpay = require('razorpay');
 require('dotenv').config(); // Load environment variables
-
+const express = require('express');
 const app = express();
-app.use(express.json()); // For parsing application/json
+const Razorpay = require('razorpay');
 
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
 // const razorpayInstance = new Razorpay({
 //   key_id: process.env.RAZORPAY_KEY_ID, // Use environment variables
 //   key_secret: process.env.RAZORPAY_KEY_SECRET,
 // });
 app.get("/",(req,res)=>{
-    console.log("chal raha hai");
-    res.send("chal raha hai");
+    res.render("index");
 })
+
+app.get("/privacy_policy",(req,res)=>{
+  res.render("privacy_policy");
+})
+
+app.get("/terms",(req,res)=>{
+  res.render("terms");
+})
+
+app.get("/refund",(req,res)=>{
+  res.render("refund");
+})
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
